2efe300c3b59fdfb8d187558ecac7e2d0b377985
leptone <leptone55@gmail.com>
2017-02-05 20:06:15 -0800

    trouble shooting

--- assets/js/volume.js
+++ assets/js/volume.js
@@ -1,10 +1,14 @@
 var preVid = document.getElementById('precomp')
+console.log('Hello')
+
 document.onload = function(e){
     console.log('e', e)
     preVid.volume = 0
 }
 
+/*
 var volLevel = document.getElementById('volume-slider')
 volLevel.onchange = function (e) {
     preVid.volume = volLevel.value
 }       
+*/
