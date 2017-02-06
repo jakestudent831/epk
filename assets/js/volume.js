271ed8aa34fbe5f3ba47b2e7c4dae6b87c5d45f7
leptone <leptone55@gmail.com>
2017-02-05 20:59:12 -0800

    dont trip

--- assets/js/volume.js
+++ assets/js/volume.js
@@ -4,14 +4,14 @@ var volLevel = document.getElementById('volume-slider')
 volLevel.onchange = function (e) {
     console.log('preVid.volume', preVid.volume)
     console.log('volLevel.value', volLevel.value)
-	preVid.muted = false
+	//preVid.muted = false
     preVid.volume = volLevel.value
-    if (volLevel.value === 0) preVid.volume = true
+    //if (volLevel.value === 0) preVid.volume = true
 }       
 
 document.onload = function(e){
-	preVid.muted = true
-    //preVid.volume = 0
+	//preVid.muted = true
+    preVid.volume = 0
 }
 
 
