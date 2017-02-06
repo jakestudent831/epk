79a5d27553624c54fafffb40d14896ceec805287
leptone <leptone55@gmail.com>
2017-02-05 20:12:58 -0800

    removed dummy code

--- assets/js/volume.js
+++ assets/js/volume.js
@@ -1,19 +1,10 @@
 var preVid = document.getElementById('precomp')
-console.log('Hello')
 
 document.onload = function(e){
-    console.log('test')
-    console.log('e', e)
     preVid.volume = 0
 }
 
-document.getElementById('reveal').onclick = function (e) {
-    console.log('click e', e)
-}
-
-/*
 var volLevel = document.getElementById('volume-slider')
 volLevel.onchange = function (e) {
     preVid.volume = volLevel.value
 }       
-*/
