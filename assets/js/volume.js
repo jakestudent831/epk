7ef1f373b0c3a03388bef2801e6f2b449da0d0f8
leptone <leptone55@gmail.com>
2017-02-05 21:30:19 -0800

    it works!

--- assets/js/volume.js
+++ assets/js/volume.js
@@ -2,16 +2,10 @@ var preVid = document.getElementById('precomp')
 
 var volLevel = document.getElementById('volume-slider')
 volLevel.onchange = function (e) {
-    console.log('preVid.volume', preVid.volume)
-    console.log('volLevel.value', volLevel.value)
-	//preVid.muted = false
+	preVid.muted = false
     preVid.volume = volLevel.value
-    //if (volLevel.value === 0) preVid.volume = true
+    if (volLevel.value === 0) preVid.volume = true
 }       
 
-document.onload = function(e){
-	//preVid.muted = true
-    preVid.volume = 0
-}
 
 
