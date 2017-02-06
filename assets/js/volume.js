0cb4f8197ea65c36d5047f52d5600432a063cda1
leptone <leptone55@gmail.com>
2017-02-05 19:49:02 -0800

    adding volume changing script

---
+++ assets/js/volume.js
@@ -0,0 +1,9 @@
+var preVid = document.getElementById('precomp')
+document.onload = function(e){
+    preVid.volume = 0
+}
+
+var volLevel = document.getElementById('volume-slider')
+volLevel.onchange = function (e) {
+    preVid.volume = volLevel.volume
+}       
